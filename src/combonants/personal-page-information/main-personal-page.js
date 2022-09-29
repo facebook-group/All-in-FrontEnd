1ea2973cd8cdc4de11787ba390a06880f7784017
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-09-29 18:39:46 +0300

    lith

--- src/combonants/personal-page-information/main-personal-page.js
+++ src/combonants/personal-page-information/main-personal-page.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from "react"
 import { useDispatch,useSelector } from "react-redux"
 import { GetAllUserData } from "../../redux/GetAllInfromationUser"
+import HerderPersonal_page_information from "./personal-page/header-personal-page";
+
 
 import "./style/personalpage.scss"
 
-
-
-import HerderPersonal_page_information from "./personal-page/header-personal-page"
-import Edite_personal_page from "./personal-page/edite-personal-page"
-import Mypostes_personal from "./personal-page/mypostes-personal"
-import Myfrind_Personal from "./personal-page/myfrind-personal"
 
 let mydata
 if(window.localStorage.mydata){
@@ -29,14 +25,6 @@ const Personal_page_information=()=>{
         <div className="main-page-holder">
           {personal_data!==false?<HerderPersonal_page_information personal_data={personal_data.payload.data[0]}/>:<></>}
           <ul className="element1">
-            <li>
-                {/* {personal_data!==false?<Edite_personal_page personal_data={personal_data.payload.data[0]}/>:<></>} */}
-                {personal_data!==false?<Myfrind_Personal personal_data={personal_data.payload.data[0].frinds}/>:<></>}
-            </li>
-            <li>
-                {personal_data!==false?<Mypostes_personal personal_data={personal_data.payload.data[0].posts}/>:<></>}
-
-            </li>
           </ul>
         </div>
     )
--- src/combonants/personal-page-information/personal-page/header-personal-page.js
+++ src/combonants/personal-page-information/personal-page/header-personal-page.js
@@ -1,95 +1,51 @@
 import React from "react"
-import Button from '@mui/material/Button';
 import { useNavigate } from "react-router";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import PublicIcon from '@mui/icons-material/Public';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 
 
 const HerderPersonal_page_information=({personal_data})=>{
-    const Navi=useNavigate()
-    const editeInformation=()=>{
-        Navi("/editeInformation")
-    }
+  const {image,fullName,email,languageSpeak,phoneNumber,place,study}=personal_data
+  const Navi=useNavigate();
+
+  const editePersonal=(e)=>{
+    e.preventDefault();
+    Navi("/editeInformation")
+  }
 
     return(
-        <>
-       
-
-<div class="wrapper">
-  <div class="profile-card js-profile-card">
-    <div class="profile-card__img">
-    <img src={personal_data.image} alt="" />
-    </div>
-
-    <div class="profile-card__cnt js-profile-cnt">
-      <div class="profile-card__name"><p>{personal_data.fullName}</p></div>
-      <div class="profile-card__txt">{personal_data.study} from <strong>{personal_data.place}</strong></div>
-      <div class="profile-card-loc">
-        <span class="profile-card-loc__icon">
-          <svg class="icon"></svg>
-        </span>
-    
-
-        <span class="profile-card-loc__txt">
-        <p>{personal_data.email}</p>
-        </span>
-
-        
-
-       
-      </div>
-
-      <div class="profile-card-loc">
-      <span class="profile-card-loc__txt">
-        <p>{personal_data.phoneNumber}</p>
-        </span>
-      </div>
-
-   
-
-        <div class="profile-card-inf__item">
-          <div class="profile-card-inf__title">{personal_data.frinds.length}</div>
-          <div class="profile-card-inf__txt">Friends</div>
-        </div>
-
-        <div class="profile-card-inf__item">
-          <div class="profile-card-inf__title">12</div>
-          <div class="profile-card-inf__txt">Posts</div>
-        </div>
-
- 
-
-        
-      <div class="profile-card-ctr">
-        <a class="profile-card__button button--blue js-message-btn" href ="http://localhost:3000/home">Home</a>
-       <a class="profile-card__button button--orange" href="http://localhost:3000/editeInformation">Edit</a>
-      </div>
-    </div>
-
-    <div class="profile-card-message js-message">
-      <form class="profile-card-form">
-        <div class="profile-card-form__container">
-          <textarea placeholder="Say something..."></textarea>
-        </div>
-
-        <div class="profile-card-form__bottom">
-          <button class="profile-card__button button--blue js-message-close">
-            Send
-          </button>
-
-          <button class="profile-card__button button--gray js-message-close">
-            Cancel
+        <div className="personal-page-container">
+          <img src={image} alt=""  className="personalImage"/>
+          <p className="personName">{fullName}</p>
+          <ul className="collection-text">
+            <li>
+              <span><AlternateEmailIcon/> </span>
+              <p> Email User : {email}</p>
+            </li>
+            <li>
+              <span><GTranslateIcon/> </span>
+              <p> language speak : {languageSpeak}</p>
+            </li>
+            <li>
+              <span><PhoneAndroidIcon/> </span>
+              <p> Phone Number : {phoneNumber}</p>
+            </li>
+            <li>
+              <span><PublicIcon/> </span>
+              <p> country Place : {place}</p>
+            </li>
+            <li>
+              <span><LocalLibraryIcon/> </span>
+              <p> Study : {study}</p>
+            </li>
+          </ul>
+          <button className="button-section" onClick={editePersonal}>
+            Edite Personal Information
           </button>
         </div>
-      </form>
-
-      <div class="profile-card__overlay js-message-close"></div>
-    </div>
-
-  </div>
-
-</div>
-
-
-         </>
     )
 }
 
