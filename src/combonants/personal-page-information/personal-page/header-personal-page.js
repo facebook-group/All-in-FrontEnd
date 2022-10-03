024055ccf0180d835b0fd2af00622899af263f26
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-10-03 09:43:50 +0300

    lith

--- src/combonants/personal-page-information/personal-page/header-personal-page.js
+++ src/combonants/personal-page-information/personal-page/header-personal-page.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useNavigate } from "react-router";
 
 
@@ -9,18 +9,29 @@ import {FcAddressBook} from "react-icons/fc";
 import {FcPhoneAndroid} from "react-icons/fc";
 import {FcGlobe} from "react-icons/fc";
 import {FcEditImage} from "react-icons/fc"
+import { RegusterId_Create_Context } from "../../context-api/personal-page";
 
 
 const HerderPersonal_page_information=({personal_data})=>{
-  const {image,fullName,email,languageSpeak,phoneNumber,place,study}=personal_data
+  const {image,fullName,email,languageSpeak,phoneNumber,place,study,frinds  }=personal_data
   const Navi=useNavigate();
+  const RegusterContext=useContext(RegusterId_Create_Context)
 
   const editePersonal=(e)=>{
     e.preventDefault();
     Navi("/editeInformation")
   }
 
+
+  const gotoFrind=(e)=>{
+    RegusterContext.setRegusterId(e.currentTarget.getAttribute("datatype"));
+    window.localStorage.saveReguster=e.currentTarget.getAttribute("datatype")
+
+    window.location.href="/personalPage"
+  }
+
     return(
+      <>
         <div className="personal-page-container">
           <img src={image} alt=""  className="personalImage"/>
           <ul className="collection-text">
@@ -53,10 +64,27 @@ const HerderPersonal_page_information=({personal_data})=>{
               </div>
             </li>
           </ul>
+
+
+            <div className="all-frind-container">
+              {frinds.length>0?frinds.map(({image,myfrindid},i)=>(
+                <div key={i} datatype={myfrindid} onClick={gotoFrind}>
+                  <img src={image} alt="" />
+                </div>
+
+              )):<></>}
+
+            </div>
+
           <button className="button-section" onClick={editePersonal}>
             Edite Personal Information
           </button>
         </div>
+
+
+
+
+        </>
     )
 }
 
